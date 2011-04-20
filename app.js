014065ec38c46bf6723a36be2527ee914ef18b00
Martin Angers <martin.n.angers@gmail.com>
2011-04-19 22:36:55 -0400

    Working web socket client and server, working client viewtext.org call.

---
+++ app.js
@@ -0,0 +1,59 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var io = require('socket.io');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'behemoth' }));
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Bookify!'
+  });
+});
+
+// Only listen on $ node app.js
+
+if (!module.parent) {
+  app.listen(9346);
+  console.log("Express server listening on port %d", app.address().port);
+}
+
+// socket.io, doc here: https://github.com/LearnBoost/Socket.IO-node
+var socket = io.listen(app); 
+socket.on('connection', function(client){ 
+  // new client is here! 
+  client.on('message', function(msg){ 
+    console.log(msg);
+    this.send('Message reçu!'); 
+  }); 
+  client.on('disconnect', function(){ 
+    console.log('Déconnecté...'); 
+  }); 
+}); 
